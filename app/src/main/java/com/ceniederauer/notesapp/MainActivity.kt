d8a15e3036510adf42a85b67fb4347273a1b54db
ceniederauer <cerodrigues@inf.ufsm.br>
2023-08-15 22:39:06 -0300

    start

---
+++ app/src/main/java/com/ceniederauer/notesapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ceniederauer.notesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ceniederauer.notesapp.ui.theme.NotesAppTheme
+
+class MainActivity : ComponentActivity() {
+
+}
